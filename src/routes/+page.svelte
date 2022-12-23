a73398b18661a054f10750adc92a4b1c65676916
Alumist <107285387+Alumist@users.noreply.github.com>
2022-12-22 21:23:04 -0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+</script>
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit my <a href="https://twitter.com/alumist_">twitter</a> to support me and follow.</p>
+    
+</main>
